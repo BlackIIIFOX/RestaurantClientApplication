08baf91d96e6183cb1ccb793bc61cb4cf2b75778
Paul Rozhkin <blackiiifox@gmail.com>
2020-05-07 22:52:48 +0300

    Auth fixed. Mock deleted.

--- app/src/main/java/com/tamagotchi/restaurantclientapplication/data/repositories/RestaurantsRepository.java
+++ app/src/main/java/com/tamagotchi/restaurantclientapplication/data/repositories/RestaurantsRepository.java
@@ -43,39 +43,7 @@ public class RestaurantsRepository {
      * @return Наблюдаемая коллекция ресторанов.
      */
     public Single<List<RestaurantModel>> getAllRestaurants() {
-        ArrayList<RestaurantModel> array = new ArrayList<>();
-        // mock restaurants.
-        RestaurantModel restaurantGallery = new RestaurantModel();
-        restaurantGallery.setAddress("Лиговский пр., 30 А, 5 этаж");
-        restaurantGallery.setPositionLatitude(59.928200);
-        restaurantGallery.setPositionLongitude(30.360137);
-        restaurantGallery.setId(1);
-        restaurantGallery.setCardPaymentPresent(true);
-        restaurantGallery.setParkingPresent(false);
-        restaurantGallery.setWifiPresent(true);
-        restaurantGallery.setPhotos(Arrays.asList(1, 2, 3, 4, 5, 6));
-        array.add(restaurantGallery);
-
-        RestaurantModel restaurantRuby = new RestaurantModel();
-        restaurantRuby.setAddress("Рубинштейна.");
-        restaurantRuby.setPositionLatitude(59.932044);
-        restaurantRuby.setPositionLongitude(30.346082);
-        restaurantRuby.setId(2);
-        restaurantRuby.setCardPaymentPresent(false);
-        restaurantRuby.setParkingPresent(true);
-        restaurantRuby.setWifiPresent(false);
-        restaurantRuby.setPhotos(Arrays.asList(7, 9));
-        array.add(restaurantRuby);
-
-        return Single.fromObservable(Observable.fromArray(array));
-    }
-}
-
-        // TODO: вернуть
-        /*return Single.create(source ->
-=======
         return Single.create(source ->
->>>>>>> master:app/src/main/java/com/tamagotchi/restaurantclientapplication/data/repositories/RestaurantsRepository.java
                 this.restaurantsApiService.getAllRestaurants()
                         .subscribeOn(Schedulers.io())
                         .observeOn(AndroidSchedulers.mainThread())
@@ -99,7 +67,7 @@ public class RestaurantsRepository {
                         )
         );
     }
-*//*
+
     public Single<RestaurantModel> getRestaurantById(Integer id) {
         return Single.create(source ->
                 this.restaurantsApiService.getRestaurantById(id)
@@ -127,4 +95,3 @@ public class RestaurantsRepository {
         );
     }
 }
-*/
--- tamagotchiserverprotocol/src/main/java/com/tamagotchi/tamagotchiserverprotocol/routers/IAccountApiService.java
+++ tamagotchiserverprotocol/src/main/java/com/tamagotchi/tamagotchiserverprotocol/routers/IAccountApiService.java
@@ -3,13 +3,13 @@ package com.tamagotchi.tamagotchiserverprotocol.routers;
 import com.tamagotchi.tamagotchiserverprotocol.models.UserModel;
 
 import io.reactivex.rxjava3.core.Single;
-import retrofit2.http.POST;
+import retrofit2.http.GET;
 
 public interface IAccountApiService {
     /**
      * Get information about account by token.
      * @return Account information.
      */
-    @POST("account")
+    @GET("account")
     Single<UserModel> getCurrentAccount();
 }
