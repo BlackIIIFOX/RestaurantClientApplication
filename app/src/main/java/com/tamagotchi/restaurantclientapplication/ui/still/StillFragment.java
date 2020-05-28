21a32442b03cb5f6f1d6ecd3cba3925f7f759175
Kirill <kirill1997_markin@mail.ru>
2020-05-29 00:09:38 +0300

    Create still UI

--- app/src/main/java/com/tamagotchi/restaurantclientapplication/ui/still/StillFragment.java
+++ app/src/main/java/com/tamagotchi/restaurantclientapplication/ui/still/StillFragment.java
@@ -13,17 +13,24 @@ import androidx.fragment.app.Fragment;
 import com.tamagotchi.restaurantclientapplication.R;
 import com.tamagotchi.restaurantclientapplication.ui.main.MainViewModel;
 import com.tamagotchi.restaurantclientapplication.ui.main.MainViewModelFactory;
+import com.tamagotchi.restaurantclientapplication.ui.main.Navigation;
 
 import androidx.annotation.Nullable;
 
 public class StillFragment extends Fragment {
 
+    private View stillFragment;
     private MainViewModel viewModel;
+
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         viewModel = new ViewModelProvider(this, new MainViewModelFactory()).get(MainViewModel.class);
+        stillFragment = inflater.inflate(R.layout.fragment_still, container, false);
 
-        return inflater.inflate(R.layout.fragment_still, container, false);
+
+        return stillFragment;
     }
+
+
 }
